8ccbbb549b01fcd41cb442a45a0a38f40d434c6e
Veer Nandan <veernandan00u@gmail.com>
2025-02-13 00:02:01 +0530

    learned a bit of routing

---
+++ ReactRouter/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+import Home from "./components/Home/Home"
+function App() {
+  
+
+  return (
+    <>
+
+      <Header></Header>
+      <Home></Home>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
