a002b2b5714ab4c46d396445d688c0ebd56611fa
Veer Nandan <veernandan00u@gmail.com>
2025-02-18 01:12:11 +0530

    no chnage from last

--- ContextAPI/Project2/src/App.jsx
+++ ContextAPI/Project2/src/App.jsx
@@ -1,44 +1,50 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 import { useState } from "react";
-import {TodoProvider} from "./Context/TodoContext"
+import { TodoProvider } from "./Context/TodoContext";
 
-  function App() {
+function App() {
+  const [todos, setTodos] = useState([]);
 
-      const [todo,setTodo] = useState([]);
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
 
-      const addTodo = (todo) => {
-        setTodo((prev)=>[...prev,{id : Date.now() , ...todo}]);
-      }
-      
-      const updateTodo = (id, todo) => {
-        setTodo((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo )
-      ))}
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
+  };
 
-      const toggleTodo = (id) => {
-        setTodo((prev) => prev.map((prevTodo) => prevTodo === id ? {...prevTodo , completed : !prevTodo.completed} : prevTodo))
-      }
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
 
-      const deleteTodo = (todo) =>{
-        setTodo((prev) => prev.filter((todo) => todo.id !== id))
-      }
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
 
-      return (
-        <TodoProvider value={{todos,addTodo,deletTodo,toggleTodo,updateTodo}}>
-          <div className="bg-[#172842] min-h-screen py-8">
-            <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-              <h1 className="text-2xl font-bold text-center mb-8 mt-2">
-                Manage Your Todos
-              </h1>
-              <div className="mb-4">{/* Todo form goes here */}</div>
-              <div className="flex flex-wrap gap-y-3">
-                {/*Loop and Add TodoItem here */}
-              </div>
-            </div>
+  return (
+    <TodoProvider value={{ todos, addTodo, deletTodo, toggleTodo, updateTodo }}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
           </div>
-        </TodoProvider>
-      );
-    }
-  
+        </div>
+      </div>
+    </TodoProvider>
+  );
+}
 
 export default App;
