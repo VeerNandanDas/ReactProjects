26f36355e7c554edbe89d665da6272224f13b2e7
Veer Nandan <veernandan00u@gmail.com>
2025-03-26 22:54:17 +0530

    Your commit message

--- DocsApp/src/App.jsx
+++ DocsApp/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div className='w-full h-screen bg-zinc-800 '>
-      <h1 className='text-white text-[12vw] leading-none tracking-tight'>Docs</h1>
+      <h1 className='text-white text-[12vw] leading-none tracking-tight top-1/2'>Docs</h1>
     </div>
   )
 }
