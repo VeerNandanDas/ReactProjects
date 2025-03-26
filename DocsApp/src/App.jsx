b031d9ee6c0dcf494ff19a1bfe04d2af31c65afd
Veer Nandan <veernandan00u@gmail.com>
2025-03-26 22:33:31 +0530

    initiated react revision

---
+++ DocsApp/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='w-full h-screen bg-zinc-800 '>
+      <h1 className='text-white text-[12vw] leading-none tracking-tight'>Docs</h1>
+    </div>
+  )
+}
+
+export default App
